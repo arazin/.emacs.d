4e5ab6cd371bc9d64c2a2d4be400aaa924cec967
arazin <hoge@hoge.hoge>
2014-10-08 00:19:34 +0900

    flycheck,smartrep

--- init.el
+++ init.el
@@ -231,87 +231,115 @@
         ("ghostview\\lgv".".ps")
         ("evince" . ".pdf")))
 
-;; flymake
-(require 'flymake)
-(defun flymake-cc-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-										 'flymake-create-temp-inplace))
-				 (local-file (file-relative-name
-											temp-file
-											(file-name-directory buffer-file-name))))
-    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" "-stf=c++11" local-file))))
-(push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
-(push '("\\.c$" flymake-cc-init) flymake-allowed-file-name-masks)
-(defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-  (setq flymake-check-was-interrupted t))
-(ad-activate 'flymake-post-syntax-check)
+;; smartrep キーバインドの複数回入力を省く
+(require 'smartrep)
 
-;; flymake popup
-(global-set-key (kbd "M-e") 'flymake-goto-next-error)
-(global-set-key (kbd "M-E") 'flymake-goto-prev-error)
-;; Show error message under current line
-(require 'popup)
-(defun flymake-display-err-menu-for-current-line ()
-  (interactive)
-  (let* ((line-no (flymake-current-line-no))
-         (line-err-info-list (nth 0 (flymake-find-err-info flymake-err-info line-no))))
-    (when line-err-info-list
-      (let* ((count (length line-err-info-list))
-             (menu-item-text nil))
-        (while (> count 0)
-          (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
-          (let* ((file (flymake-ler-file (nth (1- count) line-err-info-list)))
-                 (line (flymake-ler-line (nth (1- count) line-err-info-list))))
-            (if file
-                (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
-          (setq count (1- count))
-          (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
-          )
-        (popup-tip menu-item-text)))))
-(if (eq 'unspecified (face-attribute 'popup-tip-face :height))
-    (set-face-attribute 'popup-tip-face nil :height 1.0))
-(if (eq 'unspecified (face-attribute 'popup-tip-face :weight))
-    (set-face-attribute 'popup-tip-face nil :weight 'normal))
-(defun my/display-error-message ()
-  (let ((orig-face (face-attr-construct 'popup-tip-face)))
-    (set-face-attribute 'popup-tip-face nil
-                        :height 1.5 :foreground "firebrick"
-                        :background "LightGoldenrod1" :bold t)
-    (unwind-protect
-        (flymake-display-err-menu-for-current-line)
-      (while orig-faceDropbox/programming/myself/aoj/
-        (set-face-attribute 'popup-tip-face nil (car orig-face) (cadr orig-face))
-        (setq orig-face (cddr orig-face))))))
-(defadvice flymake-goto-prev-error (after flymake-goto-prev-error-display-message)
-  (my/display-error-message))
-(defadvice flymake-goto-next-error (after flymake-goto-next-error-display-message)
-  (my/display-error-message))
-(ad-activate 'flymake-goto-prev-error 'flymake-goto-prev-error-display-message)
-(ad-activate 'flymake-goto-next-error 'flymake-goto-next-error-display-message)
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(require 'flycheck)
+(flycheck-define-checker c/c++
+  "A C/C++ checker using g++."
+  :command ("g++" "-Wall" "-Wextra" "-std=c++11" "-fsyntax-only" source)
+  :error-patterns  ((error line-start
+                           (file-name) ":" line ":" column ":" " error: " (message)
+                           line-end)
+                    (warning line-start
+                           (file-name) ":" line ":" column ":" " warning: " (message)
+                           line-end))
+  :modes (c-mode c++-mode))
 
-;; flymake for C/C++ 
-(add-hook 'c++-mode-hook
-					'(lambda ()
-						 (flymake-mode t)))
-(add-hook 'c-mode-hook
-					'(lambda ()
-						 (flymake-mode t)))
+(add-hook 'c++-mode-hook (lambda()(flycheck-select-checker 'c/c++)))
+(add-hook 'c-mode-hook (lambda()(flycheck-select-checker 'c/c++)))
+
+(eval-after-load 'flycheck
+  '(custom-set-variables
+   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+(smartrep-define-key
+    global-map "M-g" '(("M-n" . 'flycheck-next-error)
+                       ("M-p" . 'flycheck-previous-error)))
+
+;; ;; flymake
+;; (require 'flymake)
+;; (defun flymake-cc-init ()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;; 										 'flymake-create-temp-inplace))
+;; 				 (local-file (file-relative-name
+;; 											temp-file
+;; 											(file-name-directory buffer-file-name))))
+;;     (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" "-stf=c++11" local-file))))
+;; (push '("\\.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
+;; (push '("\\.c$" flymake-cc-init) flymake-allowed-file-name-masks)
+;; (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+;;   (setq flymake-check-was-interrupted t))
+;; (ad-activate 'flymake-post-syntax-check)
+
+;; ;; flymake popup
+;; (global-set-key (kbd "M-e") 'flymake-goto-next-error)
+;; (global-set-key (kbd "M-E") 'flymake-goto-prev-error)
+;; ;; Show error message under current line
+;; (require 'popup)
+;; (defun flymake-display-err-menu-for-current-line ()
+;;   (interactive)
+;;   (let* ((line-no (flymake-current-line-no))
+;;          (line-err-info-list (nth 0 (flymake-find-err-info flymake-err-info line-no))))
+;;     (when line-err-info-list
+;;       (let* ((count (length line-err-info-list))
+;;              (menu-item-text nil))
+;;         (while (> count 0)
+;;           (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
+;;           (let* ((file (flymake-ler-file (nth (1- count) line-err-info-list)))
+;;                  (line (flymake-ler-line (nth (1- count) line-err-info-list))))
+;;             (if file
+;;                 (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
+;;           (setq count (1- count))
+;;           (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
+;;           )
+;;         (popup-tip menu-item-text)))))
+;; (if (eq 'unspecified (face-attribute 'popup-tip-face :height))
+;;     (set-face-attribute 'popup-tip-face nil :height 1.0))
+;; (if (eq 'unspecified (face-attribute 'popup-tip-face :weight))
+;;     (set-face-attribute 'popup-tip-face nil :weight 'normal))
+;; (defun my/display-error-message ()
+;;   (let ((orig-face (face-attr-construct 'popup-tip-face)))
+;;     (set-face-attribute 'popup-tip-face nil
+;;                         :height 1.5 :foreground "firebrick"
+;;                         :background "LightGoldenrod1" :bold t)
+;;     (unwind-protect
+;;         (flymake-display-err-menu-for-current-line)
+;;       (while orig-faceDropbox/programming/myself/aoj/
+;;         (set-face-attribute 'popup-tip-face nil (car orig-face) (cadr orig-face))
+;;         (setq orig-face (cddr orig-face))))))
+;; (defadvice flymake-goto-prev-error (after flymake-goto-prev-error-display-message)
+;;   (my/display-error-message))
+;; (defadvice flymake-goto-next-error (after flymake-goto-next-error-display-message)
+;;   (my/display-error-message))
+;; (ad-activate 'flymake-goto-prev-error 'flymake-goto-prev-error-display-message)
+;; (ad-activate 'flymake-goto-next-error 'flymake-goto-next-error-display-message)
+
+;; ;; flymake for C/C++ 
+;; (add-hook 'c++-mode-hook
+;; 					'(lambda ()
+;; 						 (flymake-mode t)))
+;; (add-hook 'c-mode-hook
+;; 					'(lambda ()
+;; 						 (flymake-mode t)))
 
 
-;; flymake for php
-(defun flymake-php-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "php" (list "-l" local-file))))
-(push '(".+\\.php$" flymake-php-init) flymake-allowed-file-name-masks)
-(push '("(Parse|Fatal) error: (.*) in (.*) on line ([0-9]+)" 3 4 nil 2) flymake-err-line-patterns)
+;; ;; flymake for php
+;; (defun flymake-php-init ()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                      'flymake-create-temp-inplace))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name))))
+;;     (list "php" (list "-l" local-file))))
+;; (push '(".+\\.php$" flymake-php-init) flymake-allowed-file-name-masks)
+;; (push '("(Parse|Fatal) error: (.*) in (.*) on line ([0-9]+)" 3 4 nil 2) flymake-err-line-patterns)
 
-(add-hook 'web-mode-hook
-					'(lambda ()
-						 (flymake-mode t)))
+;; (add-hook 'web-mode-hook
+;; 					'(lambda ()
+;; 						 (flymake-mode t)))
 
 ;;html php multi mode web-mode
 (require 'web-mode)
